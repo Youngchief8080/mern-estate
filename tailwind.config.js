21fa408bcd105141aa16e275403b96ea09b968df
Akpe Stanley <chatwithakpe@gmail.com>
2024-02-10 23:09:38 +0100

    installing react and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+// copy the tailwind config from the site and remove the formal one
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
